f4f409b308fcbd82ac1a2466f7cbc268d210fba0
dfang10 <dfang10@ucsc.edu>
2025-11-09 01:36:04 +0000

    Added plan.md file

---
+++ plan.md
@@ -0,0 +1,35 @@
+# D3: World of bits
+
+# Game Design Vision
+
+Game where you collect different items around campus. Combining duplicate items result in higher tiers.
+
+# Technologies
+
+- TypeScript for most game code, little to no explicit HTML, and all CSS collected in common `style.css` file
+- Deno and Vite for building
+- GitHub Actions + GitHub Pages for deployment automation
+
+# Assignments
+
+## D3.a: Core mechanics (token collection and crafting)
+
+Key technical challenge: Can you assemble a map-based user interface using the Leaflet mapping framework?
+Key gameplay challenge: Can players collect and craft tokens from nearby locations to finally make one of sufficiently high value?
+
+### Steps
+
+- [x] create plain.md file
+- [x] copy main.ts to reference.ts for future reference
+- [x] delete everything in main.ts
+- [x] put a basic leaflet map on the screen
+- [ ] draw the player's location on the map
+- [ ] draw a rectangle representing one cell on the map
+- [ ] use loops to draw a whole grid of cells on the map
+- ...
+
+## D3.b: ...
+
+...
+
+### 
--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,6 @@ import "./style.css"; // student-controlled page style
 // Fix missing marker images
 import "./_leafletWorkaround.ts"; // fixes for missing Leaflet images
 
-// Import our luck function
-import luck from "./_luck.ts";
-
 // Create basic UI elements
 
 const controlPanelDiv = document.createElement("div");
@@ -33,9 +30,6 @@ const CLASSROOM_LATLNG = leaflet.latLng(
 
 // Tunable gameplay parameters
 const GAMEPLAY_ZOOM_LEVEL = 19;
-const TILE_DEGREES = 1e-4;
-const NEIGHBORHOOD_SIZE = 8;
-const CACHE_SPAWN_PROBABILITY = 0.1;
 
 // Create the map (element with id "map" is defined in index.html)
 const map = leaflet.map(mapDiv, {
@@ -55,61 +49,3 @@ leaflet
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   })
   .addTo(map);
-
-// Add a marker to represent the player
-const playerMarker = leaflet.marker(CLASSROOM_LATLNG);
-playerMarker.bindTooltip("That's you!");
-playerMarker.addTo(map);
-
-// Display the player's points
-let playerPoints = 0;
-statusPanelDiv.innerHTML = "No points yet...";
-
-// Add caches to the map by cell numbers
-function spawnCache(i: number, j: number) {
-  // Convert cell numbers into lat/lng bounds
-  const origin = CLASSROOM_LATLNG;
-  const bounds = leaflet.latLngBounds([
-    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
-    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
-  ]);
-
-  // Add a rectangle to the map to represent the cache
-  const rect = leaflet.rectangle(bounds);
-  rect.addTo(map);
-
-  // Handle interactions with the cache
-  rect.bindPopup(() => {
-    // Each cache has a random point value, mutable by the player
-    let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
-
-    // The popup offers a description and button
-    const popupDiv = document.createElement("div");
-    popupDiv.innerHTML = `
-                <div>There is a cache here at "${i},${j}". It has value <span id="value">${pointValue}</span>.</div>
-                <button id="poke">poke</button>`;
-
-    // Clicking the button decrements the cache's value and increments the player's points
-    popupDiv
-      .querySelector<HTMLButtonElement>("#poke")!
-      .addEventListener("click", () => {
-        pointValue--;
-        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-          pointValue.toString();
-        playerPoints++;
-        statusPanelDiv.innerHTML = `${playerPoints} points accumulated`;
-      });
-
-    return popupDiv;
-  });
-}
-
-// Look around the player's neighborhood for caches to spawn
-for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-    // If location i,j is lucky enough, spawn a cache!
-    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
-      spawnCache(i, j);
-    }
-  }
-}
